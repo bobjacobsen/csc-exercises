4103f277238cc52661bf4bb7754edf5c5bd5cb2f
Bob Jacobsen <jacobsen@mac.com>
2015-09-11 05:08:54 +0200

    remove use of submit for results, changing to email

--- CSC/submit.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /home/jake/results/`basename $PWD`
-chmod -f 777 /home/jake/results/`basename $PWD`
-mkdir -p /home/jake/results/`basename $PWD`/`whoami`
-chmod 733 /home/jake/results/`basename $PWD`/`whoami`
-cp *.java /home/jake/results/`basename $PWD`/`whoami`
-
