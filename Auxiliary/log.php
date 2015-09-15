ae3618fffd3cd7eaa7a25d1a13efff1190728fd2
Bob Jacobsen <jacobsen@mac.com>
2015-09-15 09:08:42 +0300

    improve logging info

--- Auxiliary/log.php
+++ Auxiliary/log.php
@@ -1,7 +1,8 @@
 <?php
 $file = fopen("log.txt","a");
 $_GET["IP"] = $_SERVER['REMOTE_ADDR'];
-$_GET["HOST"] = $_SERVER['REMOTE_HOST'];
+$_GET["REMOTE_HOST"] = $_SERVER['REMOTE_HOST'];
+$_GET["DATE"] = date("Y-m-d H:i:s");
 
 fwrite($file,print_r($_GET, true), 500);
 fclose($file);
