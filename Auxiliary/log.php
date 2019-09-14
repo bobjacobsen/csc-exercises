7e4c5dad557bdb563d53369e9f5f9029efb44794
Bob Jacobsen <jacobsen@mac.com>
2019-09-14 21:20:52 +0300

    comments

--- Auxiliary/log.php
+++ Auxiliary/log.php
@@ -1,10 +1,13 @@
 <?php
+/* This script handles CSC logging requests to the server and writes them to a local file */
 $file = fopen("log.txt","a");
+
+/* _GET has the original request, to which we add some information */
 $_GET["IP"] = $_SERVER['REMOTE_ADDR'];
 $_GET["REMOTE_HOST"] = $_SERVER['REMOTE_HOST'];
-date_default_timezone_set("UTC");
+date_default_timezone_set("UTC");               /* llog time in UTC */
 $_GET["DATE"] = date("Y-m-d H:i:s");
 
-fwrite($file,print_r($_GET, true), 500);
+fwrite($file,print_r($_GET, true), 500);        /* limited to 500 chars per request */
 fclose($file);
 ?>
